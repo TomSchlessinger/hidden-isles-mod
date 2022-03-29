237866a19c9e9682135c9d22f9a03561a49d65cf
MyShampooIsDrunk <81791731+MyShampooIsDrunk@users.noreply.github.com>
2022-03-29 17:02:43 -0400

    Initial Commit

---
+++ src/main/java/net/myshampooisdrunk/hiddenisles/client/keybinding/HiddenIslesClient.java
@@ -0,0 +1,39 @@
+package net.myshampooisdrunk.hiddenisles.client.keybinding;
+
+
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.StickyKeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.LiteralText;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+        import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
+public class HiddenIslesClient implements ClientModInitializer {
+    //public static boolean isActivated = false;
+    public static KeyBinding ability;
+
+    @Override
+    public void onInitializeClient() {
+        KeyBinding ability = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.hiddenisles.ability",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_R,
+                "category.hiddenisles.ability"
+        ));
+
+        /*ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (ability.wasPressed()) {
+                isActivated = true;
+            }
+            while (!ability.wasPressed()) {
+                isActivated = false;
+            }
+
+        });*/
+
+    }
+}
---
+++ src/main/java/net/myshampooisdrunk/hiddenisles/item/custom/ModArmorItem.java
@@ -0,0 +1,75 @@
+package net.myshampooisdrunk.hiddenisles.item.custom;
+
+import com.google.common.collect.ImmutableMap;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.LiteralText;
+import net.myshampooisdrunk.hiddenisles.client.keybinding.HiddenIslesClient;
+import net.myshampooisdrunk.hiddenisles.item.ModArmorMaterials;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.Map;
+
+public class ModArmorItem extends ArmorItem {
+    public ModArmorItem(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
+        super(material, slot, settings);
+    }
+    private final KeyBinding abilityKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.hiddenisles.ability", // The translation key of the keybinding's name
+            InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+            GLFW.GLFW_KEY_R, // The keycode of the key
+            "category.hiddenisles.ability" // The translation key of the keybinding's category.
+    ));
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if(!world.isClient()) {
+            if(entity instanceof PlayerEntity) {
+                PlayerEntity player = (PlayerEntity)entity;
+
+                if(hasFullSuitOfArmorOn(player)) {
+                    ClientTickEvents.END_CLIENT_TICK.register(client -> {
+                        while (HiddenIslesClient.ability.wasPressed()) {
+                            client.player.sendMessage(new LiteralText("Key 1 was pressed!"), false);
+                        }
+                    });
+                }
+            }
+        }
+
+        super.inventoryTick(stack, world, entity, slot, selected);
+    }
+
+    private boolean hasFullSuitOfArmorOn(PlayerEntity player) {
+        ItemStack boots = player.getInventory().getArmorStack(0);
+        ItemStack leggings = player.getInventory().getArmorStack(1);
+        ItemStack breastplate = player.getInventory().getArmorStack(2);
+        ItemStack helmet = player.getInventory().getArmorStack(3);
+
+        return !helmet.isEmpty() && !breastplate.isEmpty()
+                && !leggings.isEmpty() && !boots.isEmpty();
+    }
+
+    private boolean hasCorrectArmorOn(ArmorMaterial material, PlayerEntity player) {
+        ArmorItem boots = ((ArmorItem)player.getInventory().getArmorStack(0).getItem());
+        ArmorItem leggings = ((ArmorItem)player.getInventory().getArmorStack(1).getItem());
+        ArmorItem breastplate = ((ArmorItem)player.getInventory().getArmorStack(2).getItem());
+        ArmorItem helmet = ((ArmorItem)player.getInventory().getArmorStack(3).getItem());
+
+        return helmet.getMaterial() == material && breastplate.getMaterial() == material &&
+                leggings.getMaterial() == material && boots.getMaterial() == material;
+    }
+
+
+}
